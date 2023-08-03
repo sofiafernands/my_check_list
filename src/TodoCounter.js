e3f54a641b4cb70e8ecb3b86d9baf80d85eeeb34
Sofia Fernandes <123645624+sofiafernands@users.noreply.github.com>
2023-08-03 19:43:33 +0000

    componetizacion

---
+++ src/TodoCounter.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function TodoCounter() {
+    return (
+      <h1>
+      Has completa 3 de 5
+     </h1>
+    );
+  }
+
+  export {TodoCounter};
---
+++ src/TodoSearch.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function TodoSearch() {
+    return (
+      <input 
+      placeholder="Cortar cebolla">
+        
+      </input>
+    );
+  }
+
+  export {TodoSearch};
