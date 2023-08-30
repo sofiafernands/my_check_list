0161e736202dbbb48a8f6a9b9160ff96a7ef4262
Sofia Fernandes <123645624+sofiafernands@users.noreply.github.com>
2023-08-30 17:39:49 +0000

    continue

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-//import logo from './platzi.webp';
 import { TodoCounter } from './TodoCounter';
 import { TodoSearch } from './TodoSearch';
 import { TodoList } from './TodoList';
@@ -9,60 +8,77 @@ import { Footer } from './footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState } from 'react';
 
+//creamos nuestro propio HOOK (useLocalStorage)
+function useLocalStorage(itemName, initialValue) {
 
-const defaultTodos = [
-  {text: "Llevar al perro al veterinario" , completed : true},
-  {text: " Ir al supermercado ", completed : true},
-  {text: " Secar la ropa ", completed : false}, 
-  {text: " Tirar la basura " , completed : false},
+  const localStorageItem = localStorage.getItem(itemName);
 
-]
+  let parsedItem;
+  if (!localStorageItem) {
+    localStorage.setItem(itemName, JSON.stringify(initialValue));
+    parsedItem = [];
+  }
+  else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+
+  const [item, setItem] = useState(parsedItem);
+
+  //con esta funcion hacemos la persistencia de datos(localStorage) y seteamos los todos(setItem)
+  const saveItem = (newItem) => {
+    localStorage.setItem('TODOSVERSION1', JSON.stringify(newItem))
+    setItem(newItem)
+  }
+
+  return [item, saveItem]
+}
 
 function App() {
-  const [todos, setTodos ] = useState (defaultTodos);
+
+  const [todos, saveTodos] = useLocalStorage('TODOSVERSION1', []);
   const [searchValue, setSearchValue] = useState('')
-  const completedTodos = todos.filter(todo => !!todo.completed).length; 
+  const completedTodos = todos.filter(todo => !!todo.completed).length;
   const totalTodos = todos.length;
   const searchedTodos = todos.filter(
     (todo) => {
       const todoText = todo.text.toLowerCase();
       const searchText = searchValue.toLowerCase();
-    return todoText.includes(searchText);
-  });
+      return todoText.includes(searchText);
+    });
 
-const completeTodo = (text) => {
-  const newTodos = [...todos];
-  const todoIndex = newTodos.findIndex(
-    (todo) => todo.text === text
-  );
-  newTodos[todoIndex].completed = true;
-  setTodos(newTodos);
-};
+  const completeTodo = (text) => {
+    const newItem = [...todos];
+    const todoIndex = newItem.findIndex(
+      (todo) => todo.text === text
+    );
+    newItem[todoIndex].completed = true;
+    saveTodos(newItem);
+  };
 
-//usamos el metodo splice para borrar con su index
-const deleteTodo = (text) => {
-  const newTodos = [...todos];
-  const todoIndex = newTodos.findIndex(
-    (todo) => todo.text === text)
-    newTodos.splice(todoIndex, 1);
-    setTodos(newTodos)
-};
+  //usamos el metodo splice para borrar con su index
+  const deleteTodo = (text) => {
+    const newItem = [...todos];
+    const todoIndex = newItem.findIndex(
+      (todo) => todo.text === text)
+    newItem.splice(todoIndex, 1);
+    saveTodos(newItem)
+  };
 
   return (
     <>
       <TodoCounter completed={completedTodos} total={totalTodos} />
-      <TodoSearch 
-      searchValue={searchValue}
-      setSearchValue ={setSearchValue}
+      <TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
       />
       <TodoList>
-        {searchedTodos.map(todo => ( 
-          <TodoItem 
-          key={todo.text} 
-          text={todo.text} 
-          completed={todo.completed} 
-          onComplete={() => completeTodo(todo.text)() }
-          onDelete = {() => deleteTodo(todo.text) }
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)()}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList >
@@ -73,7 +89,5 @@ const deleteTodo = (text) => {
 
   );
 }
-
-
 
 export default App;
